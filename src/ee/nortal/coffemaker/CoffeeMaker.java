c8f9255ecc2c149458c648c0a139ff20e67b11e9
jevgenis <jeserkin@gmail.com>
2014-06-27 16:16:42 +0300

    Add implementation for add test

--- src/ee/nortal/coffemaker/CoffeeMaker.java
+++ src/ee/nortal/coffemaker/CoffeeMaker.java
@@ -1,7 +1,25 @@
 package ee.nortal.coffemaker;
 
 class CoffeeMaker {
-    public String makeBlackCoffee() {
-        return null;
+    private int waterCapacity = 600;
+    private int beanContainerCapacity = 400;
+
+    public void makeBlackCoffee() {
+        useWater();
+        useBeans();
+    }
+
+    private void useBeans() {
+        if (beanContainerCapacity < 50) {
+            throw new RuntimeException("Not enough beans");
+        }
+        beanContainerCapacity -= 50;
+    }
+
+    private void useWater() {
+        if (waterCapacity < 150) {
+            throw new RuntimeException("Not enough water");
+        }
+        waterCapacity -= 150;
     }
 }
--- src/ee/nortal/coffemaker/CoffeeMakerTest.java
+++ src/ee/nortal/coffemaker/CoffeeMakerTest.java
@@ -1,15 +1,25 @@
 package ee.nortal.coffemaker;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-
 public class CoffeeMakerTest {
-    private CoffeeMaker coffeeMaker = new CoffeeMaker();
 
-    @Test
-    public void createBlackCoffee() {
-        String coffee = coffeeMaker.makeBlackCoffee();
-        assertEquals("black coffee", coffee);
+    private CoffeeMaker coffeeMaker;
+
+    @Before
+    public void setUp() {
+        coffeeMaker = new CoffeeMaker();
+    }
+
+    private void createMany(int n) {
+        for (int i = 0; i < n; i++) {
+            coffeeMaker.makeBlackCoffee();
+        }
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void createMaxBlackCoffee() {
+        createMany(8);
     }
 }

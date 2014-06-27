1dd598c42ce29229f2763cc7ca49845acee088d1
jevgenis <jeserkin@gmail.com>
2014-06-27 15:46:27 +0300

    Add CoffeeMaker and test

---
+++ src/ee/nortal/coffemaker/CoffeeMaker.java
@@ -0,0 +1,7 @@
+package ee.nortal.coffemaker;
+
+class CoffeeMaker {
+    public String makeBlackCoffee() {
+        return null;
+    }
+}
---
+++ src/ee/nortal/coffemaker/CoffeeMakerTest.java
@@ -0,0 +1,9 @@
+package ee.nortal.coffemaker;
+
+public class CoffeeMakerTest {
+    private CoffeeMaker coffeeMaker = new CoffeeMaker();
+
+    public void createBlackCoffee() {
+        String coffee = coffeeMaker.makeBlackCoffee();
+    }
+}

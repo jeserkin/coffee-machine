48c41221ee66d4a5a6f1796e42b9af6f7a6f74bd
jevgenis <jeserkin@gmail.com>
2014-06-27 17:01:21 +0300

    Final touches

--- src/ee/nortal/coffemaker/CoffeeMaker.java
+++ src/ee/nortal/coffemaker/CoffeeMaker.java
@@ -1,25 +1,61 @@
 package ee.nortal.coffemaker;
 
 class CoffeeMaker {
-    private int waterCapacity = 600;
-    private int beanContainerCapacity = 400;
+    private static final int MAX_WATER_CAPACITY = 600;
+    private static final int MAX_BEAN_CONTAINER_CAPACITY = 400;
+    private static final int MAX_GARBAGE_CONTAINER_CAPACITY = 400;
+    private int waterCapacity = MAX_WATER_CAPACITY;
+    private int beanContainerCapacity = MAX_BEAN_CONTAINER_CAPACITY;
+    private int garbageContainerCapacity = MAX_GARBAGE_CONTAINER_CAPACITY;
 
     public void makeBlackCoffee() {
-        useWater();
-        useBeans();
-    }
-
-    private void useBeans() {
-        if (beanContainerCapacity < 50) {
-            throw new RuntimeException("Not enough beans");
+        if (isGarbageFull(50)) {
+            throw new RuntimeException("Not enough space in garbage container");
         }
-        beanContainerCapacity -= 50;
-    }
-
-    private void useWater() {
-        if (waterCapacity < 150) {
+        if (checkWater(150)) {
             throw new RuntimeException("Not enough water");
         }
-        waterCapacity -= 150;
+        if (checkBeans(50)) {
+            throw new RuntimeException("Not enough beans");
+        }
+        useWater(150);
+        useBeans(50);
+    }
+
+    private void useWater(int amount) {
+        waterCapacity -= amount;
+    }
+
+    private boolean checkWater(int amount) {
+        return waterCapacity < amount;
+    }
+
+    private void useBeans(int amount) {
+        beanContainerCapacity -= amount;
+        putGarbage(amount);
+    }
+
+    private boolean checkBeans(int amount) {
+        return beanContainerCapacity < amount;
+    }
+
+    private void putGarbage(int amount) {
+        garbageContainerCapacity -= amount;
+    }
+
+    public void refillWater() {
+        waterCapacity = MAX_WATER_CAPACITY;
+    }
+
+    public void refillBeans() {
+        beanContainerCapacity = MAX_BEAN_CONTAINER_CAPACITY;
+    }
+
+    public void emptyGarbage() {
+        garbageContainerCapacity = MAX_GARBAGE_CONTAINER_CAPACITY;
+    }
+
+    private boolean isGarbageFull(int amount) {
+        return garbageContainerCapacity < amount;
     }
 }
--- src/ee/nortal/coffemaker/CoffeeMakerTest.java
+++ src/ee/nortal/coffemaker/CoffeeMakerTest.java
@@ -12,14 +12,44 @@ public class CoffeeMakerTest {
         coffeeMaker = new CoffeeMaker();
     }
 
+    @Test(expected = RuntimeException.class)
+    public void createMaxBlackCoffee() {
+        createMany(8);
+    }
+
+    @Test
+    public void createRefillWater() {
+        createMany(3);
+        coffeeMaker.refillWater();
+        createMany(3);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void createRefillWaterAndBeans() {
+        for (int i = 0; i < 3; i++) {
+            createMany(3);
+            coffeeMaker.refillWater();
+            coffeeMaker.refillBeans();
+        }
+    }
+
+    @Test
+    public void checkError() {
+        createMany(4);
+        coffeeMaker.refillWater();
+        createMany(4);
+        coffeeMaker.refillWater();
+        coffeeMaker.refillBeans();
+        try {
+            createMany(4);
+        } catch (RuntimeException ignored) {}
+        coffeeMaker.emptyGarbage();
+        createMany(4);
+    }
+
     private void createMany(int n) {
         for (int i = 0; i < n; i++) {
             coffeeMaker.makeBlackCoffee();
         }
-    }
-
-    @Test(expected = RuntimeException.class)
-    public void createMaxBlackCoffee() {
-        createMany(8);
     }
 }

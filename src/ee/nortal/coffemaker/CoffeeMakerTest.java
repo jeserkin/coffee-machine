68aa5721f2abf9b9895b09c8d8489e6fca78c969
jevgenis <jeserkin@gmail.com>
2014-06-27 15:49:27 +0300

    Add test improvment

--- src/ee/nortal/coffemaker/CoffeeMakerTest.java
+++ src/ee/nortal/coffemaker/CoffeeMakerTest.java
@@ -1,9 +1,15 @@
 package ee.nortal.coffemaker;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CoffeeMakerTest {
     private CoffeeMaker coffeeMaker = new CoffeeMaker();
 
+    @Test
     public void createBlackCoffee() {
         String coffee = coffeeMaker.makeBlackCoffee();
+        assertEquals("black coffee", coffee);
     }
 }
